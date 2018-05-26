6d78842172e7e1bae2108f84a8dc8bfd6d36f88e
rafael <skcc321@gmail.com>
2018-05-26 22:04:24 +0300

    alpha

--- extract.rb
+++ extract.rb
@@ -8,21 +8,31 @@ EXTENTIONS = ['.html.haml', '.html.erb', '.haml', '.erb']
 @error_items = []
 
 Dir.glob("#{VIEWS_PATH}/**/*.html.haml") do |filename|
-  file = File.open(filename, 'r')
+  file = File.open(filename, 'r+')
   curr_dir = File.dirname(filename)
 
   unless filename =~ /\/_\w*\.html\.haml/
     new_file_lines = []
     file.each_line do |line|
       if line =~ ANCHOR
+        replacement = []
+        shift = line.index(ANCHOR)
+        render_args_start_position = shift + 8
+        # get shift
         # get args
-        render_args = line[(line.index(ANCHOR) + 8).. -1]
+        render_args = line[render_args_start_position .. -1]
+
+        # convert to new hashes
+        render_args.dup.scan(/(:(\w+)\s*=>)/) do |match|
+          render_args.gsub!(match.first, "#{match.last}:")
+        end
+
 
         # cleanup them
         render_args = render_args.gsub(/^\(/, '').gsub(/\)$/, '')
 
         # partial
-        partial = render_args.split(',')[0].gsub("'", '').gsub('"', '').gsub('partial: ', '').gsub(':partial => ', '').strip.gsub(/\A\//, '')
+        partial = render_args.split(',')[0].gsub("'", '').gsub('"', '').gsub('partial: ', '').strip.gsub(/\A\//, '')
 
         split_partial = partial.split('/')
         partial_path = split_partial.push("_#{split_partial.pop}").join('/')
@@ -34,34 +44,47 @@ Dir.glob("#{VIEWS_PATH}/**/*.html.haml") do |filename|
                        end
 
 
-        partial_exact_path = EXTENTIONS.find do |extention|
+        partial_ext = EXTENTIONS.find do |extention|
           File.exists?("#{partial_path}#{extention}")
         end
 
-        unless partial_exact_path
+        if partial_ext.nil?
           @error_items << [filename, partial_path]
           next
+        else
+          partial_exact_path = "#{partial_path}#{partial_ext}"
         end
 
         # args
         args = render_args.split(',')[1]
 
         unless args.nil?
+          new_file_lines << [' ' * shift, "- # UNPARTIAL - locals (begin)"].join
+
+          original = args
           args = args.strip
 
-          if args =~ /\A(:locals =>|locals:)/
-            args = args.gsub(/\A(:locals =>|locals:)/, '').strip.gsub(/\A{(\s)/, '').gsub(/(\s)}\z/, '')
+          if args =~ /\A(locals:)/
+            args = args.gsub(/\A(locals:)/, '').strip.gsub(/\A{(\s)/, '').gsub(/(\s)}\z/, '')
           end
 
           args.split(',').each do |arg|
-            # puts arg.split(' ').first
-            puts arg
+            var = arg.split(' ').first
+            new_file_lines << [' ' * shift, "- #{var.gsub(':', '')} =#{arg.gsub(var, '')}"].join
           end
+          new_file_lines << [' ' * shift, "- # UNPARTIAL - locals (end)"].join
         end
+
+        new_file_lines << [' ' * shift, "- # UNPARTIAL - partial exctract (start)"].join
+        File.open(partial_exact_path).read.each_line do |line|
+          new_file_lines << [' ' * shift, line].join
+        end
+        new_file_lines << [' ' * shift, "- # UNPARTIAL - partial exctract (end)"].join
+
       else
         new_file_lines << line
       end
     end
-    # puts new_file_lines.join("\n")
+    file.write(new_file_lines.join("\n"))
   end
 end
